ef58ccd37b6d7cd44690ccf1748ddae3ed557f6c
DannyWadafaka <117675280+DannyWadafaka@users.noreply.github.com>
2025-05-29 23:23:41 -0500

    Add files via upload

---
+++ docs/static/models/user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, name, preferred_channel, available_channels):
+        self.name = name
+        self.preferred_channel = preferred_channel
+        self.available_channels = available_channels
+
+
